252e671592f5e66a02dd7276750e6985eefaa35c
Wolliver Rodrigues Lima <86068543+akz-6@users.noreply.github.com>
2026-08-28 20:23:45 -0300

    Add competitive programming reference: 01-fundamentos/lacos.cpp

---
+++ 01-fundamentos/lacos.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        // i = 0 ... n-1
+    }
+
+    int x = n;
+    while (x > 0) {
+        x--;
+    }
+
+    return 0;
+}
