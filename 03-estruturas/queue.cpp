0daff3521bbc3d096c7a03511133ec02b0f4ce24
Wolliver Rodrigues Lima <86068543+akz-6@users.noreply.github.com>
2026-08-28 20:24:02 -0300

    Add competitive programming reference: 03-estruturas/queue.cpp

---
+++ 03-estruturas/queue.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+
+    cout << q.front() << '\n';
+    q.pop();
+
+    // FIFO: primeiro que entra, primeiro que sai.
+}
