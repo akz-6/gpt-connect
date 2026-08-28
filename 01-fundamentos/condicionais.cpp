bd39353d59f0a2e90e7815824ce54be2eb3350e2
Wolliver Rodrigues Lima <86068543+akz-6@users.noreply.github.com>
2026-08-28 20:23:43 -0300

    Add competitive programming reference: 01-fundamentos/condicionais.cpp

---
+++ 01-fundamentos/condicionais.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x; cin >> x;
+
+    if (x > 0) cout << "positivo\n";
+    else if (x < 0) cout << "negativo\n";
+    else cout << "zero\n";
+
+    return 0;
+}
