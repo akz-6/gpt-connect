5ee1d7aaa4229c29895e3b0d4e5b47e0e0178d76
Wolliver Rodrigues Lima <86068543+akz-6@users.noreply.github.com>
2026-08-28 20:23:38 -0300

    Add competitive programming reference: templates/template_competicao.cpp

---
+++ templates/template_competicao.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // Leia a entrada
+    // Resolva
+    // Imprima a resposta
+
+    return 0;
+}
