418d7d39d2bf11f2391bf4db8e94018270f7ea9c
Wolliver Rodrigues Lima <86068543+akz-6@users.noreply.github.com>
2026-08-28 20:24:35 -0300

    Add training reference: 99-cheatsheet/sintaxe_rapida.cpp

---
+++ 99-cheatsheet/sintaxe_rapida.cpp
@@ -0,0 +1,13 @@
+/*
+C++ CHEATSHEET RÁPIDO
+vector<int> v; v.push_back(x); v.pop_back(); v.size();
+stack<int> st; st.push(x); st.top(); st.pop();
+queue<int> q; q.push(x); q.front(); q.pop();
+map<int,int> mp; mp[x]++;
+set<int> s; s.insert(x); s.count(x);
+sort(v.begin(),v.end());
+for(int i=0;i<n;i++) {}
+for(int x:v) {}
+binary_search(v.begin(),v.end(),x);
+lower_bound(v.begin(),v.end(),x);
+*/
