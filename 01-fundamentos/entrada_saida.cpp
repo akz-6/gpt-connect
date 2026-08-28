6bd33f65a9c5e0733eb24f3794987b71c6d4e032
Wolliver Rodrigues Lima <86068543+akz-6@users.noreply.github.com>
2026-08-28 20:23:41 -0300

    Add competitive programming reference: 01-fundamentos/entrada_saida.cpp

---
+++ 01-fundamentos/entrada_saida.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+
+    // Linha inteira:
+    // string s;
+    // getline(cin >> ws, s);
+
+    return 0;
+}
